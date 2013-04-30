303523d8ab9847baaec9fdb30cd38d4164e1dbf5
Spyros Chatzinikolaou <Spyros@Spyross-Mac-mini.local>
2013-04-30 16:05:10 +0300

    Moularia Commit

---
+++ Paparia/Moularia.h
@@ -0,0 +1,13 @@
+//
+//  Moularia.h
+//  Paparia
+//
+//  Created by Spyros Chatzinikolaou on 4/30/13.
+//  Copyright (c) 2013 Birdline Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Moularia : NSObject
+
+@end
---
+++ Paparia/Moularia.m
@@ -0,0 +1,13 @@
+//
+//  Moularia.m
+//  Paparia
+//
+//  Created by Spyros Chatzinikolaou on 4/30/13.
+//  Copyright (c) 2013 Birdline Software. All rights reserved.
+//
+
+#import "Moularia.h"
+
+@implementation Moularia
+
+@end
